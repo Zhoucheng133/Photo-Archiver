c9a78bb3b8387723f58f670123f584f42242aae5
zhoucheng <zhoucheng133@outlook.com>
2025-09-05 21:09:27 +0800

    改进文本颜色控制

--- lib/main.dart
+++ lib/main.dart
@@ -29,15 +29,19 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final brightness = MediaQuery.of(context).platformBrightness;
     return MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed( 
-          seedColor: Colors.lime, 
-          brightness: brightness
-        ), 
-        useMaterial3: true,
+      theme: brightness==Brightness.dark ? ThemeData.dark().copyWith(
+        textTheme: GoogleFonts.notoSansScTextTheme().apply(
+          bodyColor: Colors.white,
+          displayColor: Colors.white, 
+        ),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.lime,
+          brightness: Brightness.dark,
+        ),
+      ) : ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lime),
         textTheme: GoogleFonts.notoSansScTextTheme(),
       ),
-      themeMode: ThemeMode.system,
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: MainWindow()
