07392a04418bb3ac3f4d16e291339b46ff9f032e
zhoucheng <zhoucheng133@outlook.com>
2025-09-06 20:21:12 +0800

    添加动态库识别

--- lib/controllers/controller.dart
+++ lib/controllers/controller.dart
@@ -1,10 +1,29 @@
+import 'dart:io';
+
+import 'package:ffi/ffi.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'dart:ffi';
+
+typedef ScanDir = Pointer<Utf8> Function(Pointer<Utf8>);
 
 class Controller extends GetxController {
+
+  late ScanDir scanDir;
+
+  void initLib(){
+    final dynamicLib=DynamicLibrary.open(Platform.isMacOS ? 'lib.dylib' : 'lib.dll');
+    scanDir=dynamicLib.lookup<NativeFunction<ScanDir>>("ScanDir").asFunction();
+  }
+
+  Controller(){
+    initLib();
+  }
+
   RxString dir="".obs;
 
   void analyseDir(String dir, BuildContext context){
-    print(dir);
+    final data=scanDir(dir.toNativeUtf8());
+    print(data);
   }
 }
