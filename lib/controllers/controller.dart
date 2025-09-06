0bf30c00c37492a9de02c93faf3d712c2ed3fc06
zhoucheng <zhoucheng133@outlook.com>
2025-09-06 19:49:21 +0800

    添加本地目录加载

--- lib/controllers/controller.dart
+++ lib/controllers/controller.dart
@@ -1,5 +1,10 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class Controller extends GetxController {
   RxString dir="".obs;
+
+  void analyseDir(String dir, BuildContext context){
+    print(dir);
+  }
 }
--- lib/views/add_view.dart
+++ lib/views/add_view.dart
@@ -1,4 +1,8 @@
+import 'package:desktop_drop/desktop_drop.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:photo_archiver/controllers/controller.dart';
 
 class AddView extends StatefulWidget {
   const AddView({super.key});
@@ -8,8 +12,36 @@ class AddView extends StatefulWidget {
 }
 
 class _AddViewState extends State<AddView> {
+
+  final Controller controller=Get.find();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return DropTarget(
+      onDragDone: (detail) async {
+        final dirPath=detail.files[0].path.replaceAll("\\", "/");
+        controller.analyseDir(dirPath, context);
+      },
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            IconButton(
+              onPressed: () async {
+                String? selectedDirectory = await FilePicker.platform.getDirectoryPath();
+                if (selectedDirectory != null && context.mounted) {
+                  controller.analyseDir(selectedDirectory, context);
+                }
+              }, 
+              icon: const Icon(Icons.add_rounded)
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: Text("添加目录或拖动目录至此"),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
