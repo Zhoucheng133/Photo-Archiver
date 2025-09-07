a29115a2d56ef57127bf6fdf1941af61119b6b5b
zhoucheng <zhoucheng133@outlook.com>
2025-09-07 20:16:48 +0800

    添加异步扫描目录

--- lib/controllers/controller.dart
+++ lib/controllers/controller.dart
@@ -1,22 +1,34 @@
 import 'dart:convert';
 import 'dart:io';
-
 import 'package:ffi/ffi.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'dart:ffi';
-
 import 'package:photo_archiver/dialog/dialogs.dart';
+
+enum ArchiveBy{
+  day,
+  month,
+  year
+}
 
 class PhotoData{
   String dir;
   String name;
-  String datetime;
+  int year;
+  int month;
+  int day;
 
-  PhotoData(this.dir, this.name, this.datetime);
+  PhotoData(this.dir, this.name, this.year, this.month, this.day);
 
   factory PhotoData.decode(Map map){
-    return PhotoData(map["dir"], map["name"], map["datetime"]);
+    DateTime dateTime = DateTime.parse(map["datetime"].replaceAll('/', '-'));
+    int year = dateTime.year;
+    int month = dateTime.month;
+    int day = dateTime.day;
+
+    return PhotoData(map["dir"], map["name"], year, month, day);
   }
 }
 
@@ -24,27 +36,27 @@ typedef ScanDir = Pointer<Utf8> Function(Pointer<Utf8>);
 
 class Controller extends GetxController {
 
-  late ScanDir scanDir;
-
-  void initLib(){
-    final dynamicLib=DynamicLibrary.open(Platform.isMacOS ? 'core.dylib' : 'core.dll');
-    scanDir=dynamicLib.lookup<NativeFunction<ScanDir>>("ScanDir").asFunction();
-  }
-
-  Controller(){
-    initLib();
-  }
-
   RxString dir="".obs;
   RxList<PhotoData> photoList=RxList([]);
+  Rx<ArchiveBy> archiveBy=Rx(ArchiveBy.month);
+  RxBool loading=false.obs;
 
-  void analyseDir(String dir, BuildContext context){
+  static List isolateScan(String dir){
+    final dynamicLib=DynamicLibrary.open(Platform.isMacOS ? 'core.dylib' : 'core.dll');
+    final ScanDir scanDir=dynamicLib.lookup<NativeFunction<ScanDir>>("ScanDir").asFunction();
     final data=scanDir(dir.toNativeUtf8()).toDartString();
-    List list=jsonDecode(data);
-    if(list.isEmpty){
+    return jsonDecode(data);
+  }
+
+  Future<void> analyseDir(String dir, BuildContext context) async {
+    loading.value=true;
+    List list=await compute(isolateScan, dir);
+    if(list.isEmpty && context.mounted){
       showErrWarnDialog(context, "无法解析文件夹", "文件夹中不含任何图片文件或者无法解析任意一个图片文件");
       return;
     }
+    loading.value=false;
+
     photoList.value=list.map((item)=>PhotoData.decode(item)).toList();
     this.dir.value=dir;
   }
