7493b55b1b2d5917a18bdba3ad05f8a6e301f61b
zhoucheng <zhoucheng133@outlook.com>
2026-03-17 20:42:26 +0800

    添加繁体中文和英文支持

--- lib/lang/en_us.dart
+++ lib/lang/en_us.dart
@@ -1,3 +1,21 @@
 const Map<String, String> enUS = {
-  
+  "about": "About",
+  "prjLink": "Project Link",
+  "license": "License",
+  'ok': "OK",
+  'language': "Language",
+
+  'addDir': 'Add directory or drag directory here',
+  'year-month-day': 'Year-Month-Day',
+  'year-month': 'Year-Month',
+  'year': 'Year',
+
+  '-photos': ' Photos',
+  'cantAnalyze': 'Cannot analyze folder',
+  'cantAnalyzeContent': 'The folder contains no image files or none of the files could be parsed.',
+  'archive': 'Organize',
+  'archiveFinish': 'Organization Complete',
+  'archiveFinishContent': 'All images have been organized by ',
+  'archiveFinishContentEnd': '',
+  'close': 'Close',
 };
--- lib/lang/zh_tw.dart
+++ lib/lang/zh_tw.dart
@@ -1,3 +1,21 @@
 const Map<String, String> zhTW = {
-  
+  "about": "關於",
+  "prjLink": "項目位址",
+  "license": "許可證",
+  'ok': "好的",
+  'language': "語言",
+
+  'addDir': '添加目錄或拖動目錄至此',
+  'year-month-day': '年-月-日',
+  'year-month': '年-月',
+  'year': '年份',
+
+  '-photos': '張相片',
+  'cantAnalyze': '無法解析資料夾',
+  'cantAnalyzeContent': '資料夾中不含任何圖片檔案或無法解析任何圖片檔案',
+  'archive': '整理',
+  'archiveFinish': '整理完成',
+  'archiveFinishContent': '已經將所有圖片檔案以',
+  'archiveFinishContentEnd': '方式整理',
+  'close': '關閉',
 };
